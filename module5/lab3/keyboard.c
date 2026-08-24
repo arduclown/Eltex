78b3c2acf737d67304406d62ced6667bbfcbeb43
arduclown <maridudkina.30@gmail.com>
2026-08-24 10:06:02 +0700

    Модуль 5, задание 3

---
+++ module5/lab3/keyboard.c
@@ -0,0 +1,122 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/tty.h>          
+#include <linux/kd.h>           
+#include <linux/vt.h>
+#include <linux/console_struct.h> 
+#include <linux/vt_kern.h>
+#include <linux/timer.h>
+#include <linux/kobject.h>      
+#include <linux/sysfs.h>        
+#include <linux/string.h>
+
+MODULE_DESCRIPTION("Module for controlling keyboard LED blinking via sysfs.");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Dudkina Mariya");
+
+struct timer_list my_timer;
+struct tty_driver *my_driver;
+static struct kobject *kbleds_kobject;
+static int led_mask = 0; 
+static int kbledstatus = 0; // current LED status
+
+#define BLINK_DELAY   HZ/5
+#define ALL_LEDS_ON   0x07
+#define RESTORE_LEDS  0xFF
+
+// print current status of led_mask
+static ssize_t led_mask_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", led_mask);
+}
+
+// update led_mask
+static ssize_t led_mask_store(struct kobject *kobj, struct kobj_attribute *attr,
+                              const char *buf, size_t count)
+{
+    int new_mask;
+    if (sscanf(buf, "%d", &new_mask) != 1)
+        return -EINVAL;
+    if (new_mask < 0 || new_mask > ALL_LEDS_ON)
+        return -EINVAL;
+    led_mask = new_mask;
+    return count;
+}
+
+
+static struct kobj_attribute led_mask_attribute = __ATTR(led_mask, 0660, led_mask_show, led_mask_store);
+
+
+static void my_timer_func(struct timer_list *ptr)
+{
+    if (kbledstatus == led_mask)
+        kbledstatus = RESTORE_LEDS; // off
+    else
+        kbledstatus = led_mask; // on (by led_mask)
+    
+    // update LED status
+    if (my_driver && vc_cons[fg_console].d && vc_cons[fg_console].d->port.tty)
+        (my_driver->ops->ioctl)(vc_cons[fg_console].d->port.tty, KDSETLED, kbledstatus);
+    
+    my_timer.expires = jiffies + BLINK_DELAY;
+    add_timer(&my_timer);
+}
+
+static int __init kbleds_init(void)
+{
+    int i, error;
+
+    printk(KERN_INFO "kbleds: loading\n");
+    printk(KERN_INFO "kbleds: fgconsole is %x\n", fg_console);
+   
+    for (i = 0; i < MAX_NR_CONSOLES; i++) {
+        if (!vc_cons[i].d)
+            break;
+        printk(KERN_INFO "kbleds: console[%i/%i] #%i, tty %lx\n", i,
+               MAX_NR_CONSOLES, vc_cons[i].d->vc_num,
+               (unsigned long)vc_cons[i].d->port.tty);
+    }
+    printk(KERN_INFO "kbleds: finished scanning consoles\n");
+
+    // get driver for console
+    if (!vc_cons[fg_console].d || !vc_cons[fg_console].d->port.tty) {
+        printk(KERN_ERR "kbleds: no valid tty found for fgconsole %d\n", fg_console);
+        return -ENODEV;
+    }
+    my_driver = vc_cons[fg_console].d->port.tty->driver;
+    printk(KERN_INFO "kbleds: tty driver initialized\n");
+
+    kbleds_kobject = kobject_create_and_add("kbleds", kernel_kobj);
+    if (!kbleds_kobject) {
+        printk(KERN_ERR "kbleds: failed to create kobject\n");
+        return -ENOMEM;
+    }
+
+    error = sysfs_create_file(kbleds_kobject, &led_mask_attribute.attr);
+    if (error) {
+        printk(KERN_ERR "kbleds: failed to create file sysfs\n");
+        kobject_put(kbleds_kobject);
+        return error;
+    }
+
+    // Set up the LED blink timer
+    timer_setup(&my_timer, my_timer_func, 0);
+    my_timer.expires = jiffies + BLINK_DELAY;
+    add_timer(&my_timer);
+
+    return 0;
+}
+
+static void __exit kbleds_cleanup(void)
+{
+    printk(KERN_INFO "kbleds: unloading...\n");
+    
+    timer_delete(&my_timer);
+    if (my_driver && vc_cons[fg_console].d && vc_cons[fg_console].d->port.tty)
+        (my_driver->ops->ioctl)(vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
+
+    kobject_put(kbleds_kobject);
+}
+
+module_init(kbleds_init);
+module_exit(kbleds_cleanup);
